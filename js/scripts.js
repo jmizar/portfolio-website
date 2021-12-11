aee6c80028df38e452984c9a40655fbf0b607685
jmizar <73902204+jmizar@users.noreply.github.com>
2021-12-11 18:38:44 +0000

    task 2.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  if (document.body.clientWidth < 641) {
+    var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
+  }
   }
 }
